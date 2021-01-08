4b988fe9d878b0d66e69da0b3f44f3c4828cdbe2
Rishabh Sharma <therishabhsharma763@gmail.com>
2021-01-09 02:05:02 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+# a = "Kuch Kuch Hota hai"
+# e = "1998"
+
+# a = a.lower()
+# a = a.split()
+
+# # c = "abv"
+# # d = "efg"
+
+# b = ""
+# for i in a:
+#     print(i)
+#     b = b + i + "-"
+    
+# b = b + e 
+# print(b)
+
+# movie_rel_date = "Oct 15 1998"
+
+# print( movie_rel_date[7:] )
+from datetime import date, datetime
+print("LINE")
+
+today = str(date.today())
+
+print(today[0:4])
